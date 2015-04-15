18e8107283ef5bc35b3c97db9309864e3e99f2ed
PMurph <murph.pm@gmail.com>
2015-04-15 18:16:43 -0500

    Unreal Engine fps template project

---
+++ TheArena/Source/TheArena/TheArena.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __THEARENA_H__
+#define __THEARENA_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ TheArena/Source/TheArena/TheArenaGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "TheArenaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheArenaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ATheArenaGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
